0bca0732a2c0819420e72a3e12a7fd3d0f5f380b
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-08-18 12:16:07 -0500

    apply routes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,11 +13,38 @@ declare var google;
 export class HomePage {
 
   map: any;
+  directionsService: any = null;
+  directionsDisplay: any = null;
+  bounds: any = null;
+  myLatLng: any;
+  waypoints: any[];
 
   constructor(
     public navCtrl: NavController,
     public geolocation: Geolocation
-  ) {}
+  ) {
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsDisplay = new google.maps.DirectionsRenderer();
+    this.bounds = new google.maps.LatLngBounds();
+    this.waypoints = [
+      {
+        location: { lat: 4.6241329, lng: -74.1768411 },
+        stopover: true,
+      },
+      {
+        location: { lat: 4.6247745, lng: -74.1698888 },
+        stopover: true,
+      },
+      {
+        location: { lat: 4.6212241, lng: -74.1631081 },
+        stopover: true,
+      },
+      {
+        location: { lat: 4.6222508, lng: -74.1667989 },
+        stopover: true,
+      }
+    ];
+  }
 
   ionViewDidLoad(){
     this.getPosition();
@@ -37,26 +64,54 @@ export class HomePage {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
     console.log(latitude, longitude);
-    
     // create a new map by passing HTMLElement
     let mapEle: HTMLElement = document.getElementById('map');
+    let panelEle: HTMLElement = document.getElementById('panel');
 
     // create LatLng object
-    let myLatLng = {lat: latitude, lng: longitude};
+    this.myLatLng = {lat: latitude, lng: longitude};
 
     // create map
     this.map = new google.maps.Map(mapEle, {
-      center: myLatLng,
+      center: this.myLatLng,
       zoom: 12
     });
 
+    this.directionsDisplay.setMap(this.map);
+    this.directionsDisplay.setPanel(panelEle);
+
     google.maps.event.addListenerOnce(this.map, 'idle', () => {
-      let marker = new google.maps.Marker({
-        position: myLatLng,
-        map: this.map,
-        title: 'Hello World!'
-      });
       mapEle.classList.add('show-map');
+      this.calculateRoute();
     });
+  }
+
+  private calculateRoute(){
+    
+    this.bounds.extend(this.myLatLng);
+
+    this.waypoints.forEach(waypoint => {
+      var point = new google.maps.LatLng(waypoint.location.lat, waypoint.location.lng);
+      this.bounds.extend(point);
+    });
+
+    this.map.fitBounds(this.bounds);
+
+    this.directionsService.route({
+      origin: new google.maps.LatLng(this.myLatLng.lat, this.myLatLng.lng),
+      destination: new google.maps.LatLng(this.myLatLng.lat, this.myLatLng.lng),
+      waypoints: this.waypoints,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING,
+      avoidTolls: true
+    }, (response, status)=> {
+      if(status === google.maps.DirectionsStatus.OK) {
+        console.log(response);
+        this.directionsDisplay.setDirections(response);
+      }else{
+        alert('Could not display directions due to: ' + status);
+      }
+    });  
+
   }
 }
